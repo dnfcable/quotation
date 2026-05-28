397ec99eb70d49e47f49b093b807e34a1471f12e
dnfcable <edwinlim@dnfcable.com>
2026-05-28 09:51:17 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = 'dnf-quote-v11';
+﻿const CACHE = 'dnf-quote-v12';
 const ASSETS = ['/', '/index.html', '/app.css', '/app.js', '/DNF Quotation App.html'];
 
 self.addEventListener('install', e => {
